30029682e8b59e5b76650c651f9d1b4083ee4d0b
Ayush <ayushagarwal.dev@gmail.com>
2024-07-25 22:33:10 +0530

    add product hunt label

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,12 @@ import React from "react";
 import HeroImage from "@/components/home/HeroImage";
 import Heading from "@/components/home/Heading";
 import Bullets from "@/components/home/Bullets";
+import ProductHuntLabel from "@/components/home/ProductHuntLabel";
 
 const HomePage = () => {
   return (
     <header className="w-full min-h-svh flex flex-col justify-center items-center overflow-hidden">
-      {/*<div className={"w-fit mx-auto my-10"}>*/}
-      {/*  <a*/}
-      {/*    href="https://www.producthunt.com/posts/aibud?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-aibud"*/}
-      {/*    target="_blank"*/}
-      {/*  >*/}
-      {/*    <img*/}
-      {/*      src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=465863&theme=light"*/}
-      {/*      alt="AiBud - AiBud&#0032;&#0045;&#0032;Where&#0032;Entertainment&#0032;Meets&#0032;Fashion&#0033; | Product Hunt"*/}
-      {/*      style={{ width: 250, height: 54 }}*/}
-      {/*    />*/}
-      {/*  </a>*/}
-      {/*</div>*/}
+      <ProductHuntLabel />
       <div className="container md:max-w-screen-[1200px] px-6 pb-16 mx-auto">
         <div className="items-center lg:gap-4 lg:flex">
           <div className="w-full lg:w-[60%]">
---
+++ src/components/home/ProductHuntLabel.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React, { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const ProductHuntLabel = () => {
+  const labelRef = useRef(null);
+
+  useGSAP(
+    () => {
+      gsap.from(labelRef.current, {
+        y: 100,
+        opacity: 0,
+        duration: 2.5,
+        ease: "bounce",
+      }); // <-- automatically reverted
+    },
+    { dependencies: [] }
+  );
+
+  return (
+    <div
+      className={"w-fit mx-auto mb-3 mt-12  md:mt-2 md:mb-10"}
+      ref={labelRef}
+    >
+      <a
+        href="https://www.producthunt.com/posts/aibud?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-aibud"
+        target="_blank"
+      >
+        <img
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=465863&theme=light"
+          alt="AiBud - AiBud&#0032;&#0045;&#0032;Where&#0032;Entertainment&#0032;Meets&#0032;Fashion&#0033; | Product Hunt"
+          className={"w-[150px] h-[50px]  md:w-[250px] md:h-[54px]"}
+        />
+      </a>
+    </div>
+  );
+};
+
+export default ProductHuntLabel;
